19aa7b0384bd104059635676bba3e858131a5a10
MatheusCapelari <32314560+MatheusCapelari@users.noreply.github.com>
2017-11-19 16:46:44 -0200

    Bridge

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridge : MonoBehaviour {
+
+    public Animator animator;
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.SetTrigger("Firstmove");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animator.SetTrigger("Secmove");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            animator.SetTrigger("On");
+        }
+
+    }
+}
---
+++ Assets/Scripts/ButtonSwitch.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSwitch : MonoBehaviour {
+
+    public Animator animator;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            animator.SetBool("Press", true);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            animator.SetBool("Press", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            animator.SetBool("Press", false);
+        }
+    }
+}
